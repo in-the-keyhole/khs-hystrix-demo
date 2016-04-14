aa468d2be3dbd8eb9aeff5e32d00236a16448806
Dallas Monson <dallas.monson@yrcfreight.com>
2016-04-14 08:32:02 -0500

    Fixing the http return codes to be more descriptive

--- src/main/java/com/example/controller/SampleController.java
+++ src/main/java/com/example/controller/SampleController.java
@@ -22,25 +22,25 @@ public class SampleController {
 	@RequestMapping(method = RequestMethod.GET ,produces = "application/json")
 	public ResponseEntity<List<Payload>> getAll(){
 		List<Payload> pay = service.returnAll();
-		return (pay.isEmpty()? new ResponseEntity<List<Payload>>(HttpStatus.NOT_FOUND) : new ResponseEntity<List<Payload>>(pay, HttpStatus.OK) );
+		return (pay.isEmpty()? new ResponseEntity<List<Payload>>(HttpStatus.INTERNAL_SERVER_ERROR) : new ResponseEntity<List<Payload>>(pay, HttpStatus.OK) );
 	}
 	
 	@RequestMapping(value="/hystrix", method = RequestMethod.GET ,produces = "application/json")
 	public ResponseEntity<List<Payload>> getAllHystrix(){
 		List<Payload> pay = service.returnAllHystrix();
-		return (pay.isEmpty()? new ResponseEntity<List<Payload>>(HttpStatus.NOT_FOUND) : new ResponseEntity<List<Payload>>(pay, HttpStatus.OK) );
+		return (pay.isEmpty()? new ResponseEntity<List<Payload>>(HttpStatus.SERVICE_UNAVAILABLE) : new ResponseEntity<List<Payload>>(pay, HttpStatus.OK) );
 	}
 	
 	@RequestMapping(value="/fail", method = RequestMethod.GET, produces = "application/json")
 	public ResponseEntity<List<Payload>> getFails(){
 		List<Payload> pay =  service.failAll();
-		return (pay.isEmpty()? new ResponseEntity<List<Payload>>(HttpStatus.NOT_FOUND) : new ResponseEntity<List<Payload>>(pay, HttpStatus.OK) );
+		return (pay.isEmpty()? new ResponseEntity<List<Payload>>(HttpStatus.INTERNAL_SERVER_ERROR) : new ResponseEntity<List<Payload>>(pay, HttpStatus.OK) );
 	}
 	
 	@RequestMapping(value="/fail/hystrix", method = RequestMethod.GET, produces = "application/json")
 	public ResponseEntity<List<Payload>> getFailsHystrix(){
 		List<Payload> pay = service.failAllHystrix();
-		return (pay.isEmpty()? new ResponseEntity<List<Payload>>(HttpStatus.NOT_FOUND) : new ResponseEntity<List<Payload>>(pay, HttpStatus.OK) );
+		return (pay.isEmpty()? new ResponseEntity<List<Payload>>(HttpStatus.SERVICE_UNAVAILABLE) : new ResponseEntity<List<Payload>>(pay, HttpStatus.OK) );
 	}
 	
 	
